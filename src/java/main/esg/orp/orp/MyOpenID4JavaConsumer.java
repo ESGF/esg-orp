24cc7a9f152dd0367b238a81bfb45ff4b7fafc74
kleanthis <kleanthis@mist.badc.rl.ac.uk>
2014-09-18 11:51:16 +0100

    Minor changes.

--- src/java/main/esg/orp/orp/MyOpenID4JavaConsumer.java
+++ src/java/main/esg/orp/orp/MyOpenID4JavaConsumer.java
@@ -19,7 +19,6 @@
 package esg.orp.orp;
 
 import java.net.URL;
-import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -53,7 +52,7 @@ public class MyOpenID4JavaConsumer extends OpenID4JavaConsumer {
 	/**
      * Service holding the white list of trusted Identity Providers.
      */
-    public static  RegistryService registryService;
+    private RegistryService registryService;
 	
 	/**
 	 * List of trusted IdP providers.
